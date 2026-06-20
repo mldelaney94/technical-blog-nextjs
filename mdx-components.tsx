2f42c8edead2cb720766cb1812f7c42c9b9dcdd4
Matthew Delaney <mldelaney94@gmail.com>
2026-06-20 15:40:40 +1000

    mass gainer review

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { ComponentPropsWithoutRef, JSX } from 'react'
 
-import { CodeBlock, Pre } from '@/components/code-block'
-
 // Heading component with level-based top margins
 type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6
 type HeadingTag = `h${HeadingLevel}`
@@ -31,7 +29,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h6: (props: ComponentPropsWithoutRef<'h6'>) => <Heading level={6} {...props} />,
     Image,
     Link,
-    pre: Pre,
   }
 }
 
